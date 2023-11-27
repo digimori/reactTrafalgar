dcb6c42ca2e55e2a227fa09e8442ded4433dcb13
digimori <hanaorikuu@gmail.com>
2023-11-27 13:29:58 +0000

    Vite and React-Bootstrap installed and tested.

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ButtonTest from './components/ButtonTest';
+
+function App() {
+
+  return (
+    <div>
+      <ButtonTest />
+    </div>
+
+  )
+}
+
+export default App
