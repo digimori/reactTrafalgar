23a947a32ce416d12a15ca1b7e9f7beae565205b
digimori <hanaorikuu@gmail.com>
2023-11-27 14:17:23 +0000

    Data and images populated to be used in components.

---
+++ vite-project/src/constants/images.js
@@ -0,0 +1,33 @@
+import bg from '../assets/images/bg element.png';
+import consultation from '../assets/images/consultation.png';
+import details from '../assets/images/details.png';
+import elementDots from '../assets/images/element.png';
+import emCare from '../assets/images/emCare.png';
+import illustration from '../assets/images/illustration.png';
+import articleimg1 from '../assets/images/img1.png';
+import articleimg2 from '../assets/images/image 2.png';
+import articleimg3 from '../assets/images/image 3.png';
+import logo from '../assets/images/logo.png';
+import onlinePharm from '../assets/images/onlinePharm';
+import tracker from '../assets/images/tracker.png';
+import sectionimg1 from '../assets/images/trafalgar-illustration sec02 1.png';
+import sectionimg2 from '../assets/images/trafalgar-illustration sec03 1.png';
+import searchDoc from '../assets/images/searchdoc.png'
+
+export default {
+    bg,
+    consultation,
+    details,
+    elementDots,
+    emCare,
+    illustration,
+    articleimg1,
+    articleimg2,
+    articleimg3,
+    logo,
+    onlinePharm,
+    tracker,
+    sectionimg1,
+    sectionimg2,
+    searchDoc
+}
